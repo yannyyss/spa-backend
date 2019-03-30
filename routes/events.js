5c600fa0cbc03e6bcfc87c6055b5f1bfbd61a647
yannyyss <GitYan12>
2019-03-29 19:38:40 -0600

    routes events customed

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const baseUrl = "https://date.nager.at/Api/v2/AvailableCountries";
+const countriesUrl = "https://date.nager.at/Api/v2/AvailableCountries";
 
 router.get("/all", async (req, res, next) => {
   try {
-    const response = await axios.get(baseUrl);
+    const response = await axios.get(countriesUrl);
     res.json(response.data);
   } catch (error) {
     console.log("No sirve:", error);
@@ -19,8 +19,9 @@ router.post("/holidays", async (req, res, next) => {
     console.log(req.body);
     const countryCode = req.body.countryCode;
     console.log(countryCode);
+    const yearCode = req.body.yearCode;
     const response = await axios.get(
-      `https://date.nager.at/api/v2/publicholidays/2017/${countryCode}`
+      `https://date.nager.at/api/v2/publicholidays/${yearCode}/${countryCode}`
     );
     console.log("Holidays:", response.data);
     res.json(response.data);
