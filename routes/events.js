b0574a3fcefd0a130be2928e34e163b963dc13f4
yannyyss <GitYan12>
2019-03-29 21:43:05 -0600

    Backend working and cleaned

--- routes/events.js
+++ routes/events.js
@@ -8,24 +8,21 @@ router.get("/all", async (req, res, next) => {
     const response = await axios.get(countriesUrl);
     res.json(response.data);
   } catch (error) {
-    console.log("No sirve:", error);
     res.status(400).json({ error: error });
   }
 });
 
 router.post("/holidays", async (req, res, next) => {
   try {
-    //const year = req.body.year;
-    console.log(req.body);
     const countryCode = req.body.countryCode;
-    console.log(countryCode);
     const yearCode = req.body.yearCode;
     const response = await axios.get(
       `https://date.nager.at/api/v2/publicholidays/${yearCode}/${countryCode}`
     );
-    console.log("Holidays:", response.data);
     res.json(response.data);
-  } catch (e) {}
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
 });
 
 module.exports = router;
